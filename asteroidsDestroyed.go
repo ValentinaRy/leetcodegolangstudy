07b9663b2098b665e8d616e96f1ef7c46e501b72
ValentinaRy <valeera47@gmail.com>
2026-05-31 14:37:41 +0200

    2126. Destroying Asteroids

---
+++ asteroidsDestroyed.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func asteroidsDestroyed(mass int, asteroids []int) bool {
+	sort.Ints(asteroids)
+	for i := 0; i < len(asteroids); i++ {
+		if mass >= asteroids[i] {
+			mass += asteroids[i]
+		} else {
+			return false
+		}
+	}
+	return true
+}
